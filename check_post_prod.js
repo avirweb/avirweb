242e7ae3f30431293927a7d122faa555b8690ed6
Zackor <zackor@xenovira.com>
2026-01-27 17:53:58 +0000

    chore: clean up temporary test scripts and artifacts

--- check_post_prod.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.avir.com/post/a-legacy-in-sound--801-abbey-road-limited-edition', { waitUntil: 'networkidle' });
-  const height = await page.evaluate(() => document.body.scrollHeight);
-  console.log('Prod Post height:', height);
-  await browser.close();
-})();
--- check_post_sections_legacy.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:8791/post/a-legacy-in-sound--801-abbey-road-limited-edition.html', { waitUntil: 'networkidle' });
-  const localSections = await page.evaluate(() => Array.from(document.querySelectorAll('.section')).map(s => s.className + ' | ' + s.innerText.substring(0, 50)));
-  console.log('Local Sections:', localSections);
-  await browser.close();
-})();
